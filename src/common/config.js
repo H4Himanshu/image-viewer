aeb3ab79a3d227d0cf7054e9cdac996c5c71a0c8
himanshu chawla <himanshuchawla4393@gmail.com>
2021-06-11 01:21:41 +0530

    Initial Commit

---
+++ src/common/config.js
@@ -0,0 +1,17 @@
+var Config =
+{
+    "login": {
+        "username": "admin",
+        "password": "admin"
+    },
+    "auth": {
+        "access-token": "IGQVJXQnZABc3poR1NTMERnLV9uUGJFNlY2T3B2M3ZA0MWFDaVFycHFiNmxjWFpaRmxtT2R3c0hVMEo4TmZADVzc3QkU0VkdsNGZAReVVOTDhZAVGtYaWFJbnBJd3FsN21aOEljOTRGVFRybjZAKRUdTdl9nZAwZDZD"        
+    },
+    "api": {
+        mediaUrl: "https://graph.instagram.com/me/media?fields=id,caption",
+        userUrl: "https://graph.instagram.com/me/media?fields=id,media_type,media_url,username,timestamp,caption"
+    }
+};
+export default Config;
+
+//himanshu
